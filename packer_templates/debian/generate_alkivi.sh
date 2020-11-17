fc1ca0362f58af4459581d107db710598ed9c0db
Anthony Martinet <anthony@alkivi.fr>
2020-11-17 17:40:23 +0100

    fix(all)

--- packer_templates/debian/generate_alkivi.sh
+++ packer_templates/debian/generate_alkivi.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in 3002.1; do 
+for i in 3002; do 
     for j in 9 10; do 
         echo "packer build -only=virtualbox-iso -var 'box_basename=debian-$j-salt-$i-py3' -var 'salt_version=$i' -var 'salt_python_version=py3' debian-$j-amd64-salt.json"; 
     done ; 
